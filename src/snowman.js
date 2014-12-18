0980efe88e2e9205d8e16cacdc0c8f95787f4eeb
Mikko Jakonen <mikko.j.jakonen@jyu.fi>
2014-12-18 21:07:11 +0200

    Collecting stuff and the title screen.

--- src/snowman.js
+++ src/snowman.js
@@ -33,7 +33,20 @@
         coins,
         rainbow,
         
+        //texts:
+        title,
+        instruction,
+        credits,
+        soundSetting,
+        soundClickArea,
+        
+        score = 0,
+        
+        musicEnabled = true,
+        
         //helper variables
+        _idleTween,
+        _running = false,
         _t = 0,
         _sinking = true,
         _targetRotation = 0
@@ -58,6 +71,7 @@
             character = game.add.sprite(200, 300, 'sprites', 'snowman');
             character.anchor.setTo(0, 0.5);
             game.add.tween(character).to({x: character.x + 20}, 300, Phaser.Easing.Linear.None, true, 0, -1, true);
+            _idleTween = game.add.tween(character).to({y: character.y + 30}, 1200, Phaser.Easing.Linear.None, true, 0, -1, true);
             
             //setup object groups:
             stars = game.add.group();
@@ -77,29 +91,63 @@
 
             //setup background music:
             music = game.add.audio('walking');
-            music.play('', 0, 1, true);
+            //music.play('', 0, 1, true);
             
             //setup physics:
             game.world.bringToTop(character);
             game.physics.enable(character, Phaser.Physics.ARCADE);
             
-            game.add.text(20, 20, 'JOTAIN', {font: '50px VT323', fill: 'white', align: 'center'});
-            
+            //menu texts:
+            this.setupTexts();
+
             //bind input events:
             game.input.onDown.add(this.mouseDown, this);
             game.input.onUp.add(this.mouseUp, this);
             
-            game.time.events.loop(starInterval, this.makeStars, this);
             game.time.events.loop(rainbowInterval, this.makeRainbow, this);
-            game.time.events.loop(coinInterval, this.makeCoins, this);
+            game.time.events.loop(starInterval, this.makeStars, this);
+        },
+        
+        setupTexts: function() {
+            var creditStyle = {font: '40px VT323', fill: 'white', align: 'left'};
+            var headingStyle = {font: '60px VT323', fill:'white', align: 'center'};
+            var gameStyle = {font: '25px VT323', fill:'blue', align: 'center'};
+            
+            title = game.add.text(game.world.centerX, 60, 'WALKING IN THE AIR\n THE GAME', headingStyle);
+            title.anchor.set(0.5);
+            
+            instruction = game.add.text(game.world.centerX, 180, 'HOLD AND RELEASE MOUSE/FINGER TO PLAY', gameStyle);
+            instruction.anchor.set(0.5);
+            
+            credits = game.add.text(20, game.world.height - 70, 'PROGRAMMING & GRAPHICS: MIKKO JAKONEN\nMUSIC: OSKARI JAKONEN', creditStyle);
+            
+            soundSetting = game.add.text(game.world.centerX, game.world.centerY + 180, 'MUSIC: ON  :)', gameStyle);
+            soundSetting.anchor.set(0.5);
+            soundSetting.inputEnabled = true;
+            
+            soundClickArea = new Phaser.Rectangle(soundSetting.x - 20, soundSetting.y - 20, soundSetting.width + 20, soundSetting.height + 20);
         },
         
         
+        //start the actual game from the idle menu state:
+        startGame: function() {
+            _running = true;
+            _idleTween.stop();
+            if(musicEnabled) music.play('', 0, 1, true);
+            
+            game.time.events.loop(coinInterval, this.makeCoins, this);
+            
+            game.add.tween(title).to({y: -80}, 800, Phaser.Easing.Cubic.Out, true);
+            game.add.tween(credits).to({y: game.world.height + 80}, 800, Phaser.Easing.Cubic.Out, true);
+            game.add.tween(instruction).to({alpha:0}, 2000, Phaser.Easing.Cubic.Out, true);
+            game.add.tween(soundSetting).to({alpha:0}, 800, Phaser.Easing.Linear.None, true);
+        },
         
         //================={Updating:}=================//
         
         
         update: function() {
+            if(!_running) return;
             
             character.body.velocity.y = -moveSpeed;
             
@@ -110,6 +158,8 @@
             coins.forEachAlive(function(coin) {
                 if (coin.x < -10) coin.kill();
             }, this);
+            
+            game.physics.arcade.collide(character, coins, this.collisionHandler, null, this);
         },
         
         
@@ -153,10 +203,30 @@
         },
         
         
+        collisionHandler: function(char, coin){
+            score++;
+            coin.kill();
+        },
+        
+        
         //================={Input events:}=================//
         
-        mouseDown: function(e) {
-           _sinking = false; 
+        mouseDown: function(e) {            
+            if(!_running) {
+                if(soundClickArea.contains(e.x, e.y)) {
+                    if(musicEnabled) {
+                        soundSetting.text = 'MUSIC: OFF :(';
+                        musicEnabled = false;
+                    } else {
+                        soundSetting.text = 'MUSIC: ON  :)';
+                        musicEnabled = true;
+                    }
+                    return;
+                }
+                
+                this.startGame();
+            }
+            _sinking = false; 
         },
         
         mouseUp: function(e) {
