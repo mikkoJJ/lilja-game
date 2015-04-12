e6c0277c87be249ce394e160ed99285335e78450
Mikko J Jakonen <mikko.j.jakonen@gmail.com>
2015-04-12 14:03:02 +0300

    Modifying the structure a bit.

--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
 /******************************************************
- * ================
- * The snowman game
- * ================
+ * ===========
+ * ESCAPE GAME
+ * ===========
  *          by Mikko Jakonen
  *
- * A silly little game about a flying snowman. Built
- * on the Phaser HTML5 framework: http://phaser.io
+ * A little HTML5 shmup or something.
  *****************************************************/
 
 (function(){
-
     var 
         //settings
         starInterval = 300,
@@ -26,7 +24,6 @@
         rainbowOffsetY = -15,
         
         //game entities
-        game,
         music,
         character,
         stars,
@@ -52,37 +49,34 @@
         _sinking = true,
         _targetRotation = 0
     ;
-    
-    
-    //Phaser game object
-    var snowman = {
+
+    Escape.Main = function() {
+
+    };
+
+    Escape.Main.prototype = {
         
         //================={Game setup:}=================//
-        
-        preload: function() {
-            game.load.atlas('sprites', ASSETS_FOLDER + '/walkingsprites.png', 'assets/walkingsprites_data.json');    
-            game.load.audio('walking', ASSETS_FOLDER + '/oskari_walking.ogg');
-        },
-    
+
         create: function() {
             //setup stage:
-            game.stage.backgroundColor = 0x2C59B2;
+            this.game.stage.backgroundColor = 0x2C59B2;
             
             //setup player character:
-            character = game.add.sprite(200, 300, 'sprites', 'snowman');
+            character = this.game.add.sprite(200, 300, 'sprites', 'snowman');
             character.anchor.setTo(0, 0.5);
-            game.add.tween(character).to({x: character.x + 20}, 300, Phaser.Easing.Linear.None, true, 0, -1, true);
-            _idleTween = game.add.tween(character).to({y: character.y + 30}, 1200, Phaser.Easing.Linear.None, true, 0, -1, true);
+            this.game.add.tween(character).to({x: character.x + 20}, 300, Phaser.Easing.Linear.None, true, 0, -1, true);
+            _idleTween = this.game.add.tween(character).to({y: character.y + 30}, 1200, Phaser.Easing.Linear.None, true, 0, -1, true);
             
             //setup object groups:
-            stars = game.add.group();
+            stars = this.game.add.group();
             stars.createMultiple(10, 'sprites', 'star', false);
             
-            coins = game.add.group();
+            coins = this.game.add.group();
             coins.createMultiple(20, 'sprites', 'flake', false);
             
             //setup the lovely rainbow:
-            rainbow = game.add.group();
+            rainbow = this.game.add.group();
             //rainbow.createMultiple(3, 'sprites', 'rainbow', true);
             
             for(var i=0; i < rainbowParts; i++) {
@@ -91,27 +85,27 @@
             }
 
             //setup background music:
-            music = game.add.audio('walking');
+            music = this.game.add.audio('walking');
             
             //particle setup
-            emitter = game.add.emitter(0, 0, 100);
+            emitter = this.game.add.emitter(0, 0, 100);
             emitter.makeParticles('sprites', 'ding');
             emitter.gravity = 100;
             emitter.setAlpha(1, 0, 2000);
             
             //setup physics:
-            game.world.bringToTop(character);
-            game.physics.enable(character, Phaser.Physics.ARCADE);
+            this.game.world.bringToTop(character);
+            this.game.physics.enable(character, Phaser.Physics.ARCADE);
             
             //menu texts:
             this.setupTexts();
 
             //bind input events:
-            game.input.onDown.add(this.mouseDown, this);
-            game.input.onUp.add(this.mouseUp, this);
+            this.game.input.onDown.add(this.mouseDown, this);
+            this.game.input.onUp.add(this.mouseUp, this);
             
-            game.time.events.loop(rainbowInterval, this.makeRainbow, this);
-            game.time.events.loop(starInterval, this.makeStars, this);
+            this.game.time.events.loop(rainbowInterval, this.makeRainbow, this);
+            this.game.time.events.loop(starInterval, this.makeStars, this);
         },
         
         setupTexts: function() {
@@ -119,15 +113,15 @@
             var headingStyle = {font: '60px VT323', fill:'white', align: 'center'};
             var gameStyle = {font: '25px VT323', fill:'blue', align: 'center'};
             
-            title = game.add.text(game.world.centerX, 60, 'WALKING IN THE AIR\n THE GAME', headingStyle);
+            title = this.game.add.text(this.game.world.centerX, 60, 'WALKING IN THE AIR\n THE GAME', headingStyle);
             title.anchor.set(0.5);
             
-            instruction = game.add.text(game.world.centerX, 180, 'HOLD AND RELEASE MOUSE/FINGER TO PLAY', gameStyle);
+            instruction = this.game.add.text(this.game.world.centerX, 180, 'HOLD AND RELEASE MOUSE/FINGER TO PLAY', gameStyle);
             instruction.anchor.set(0.5);
             
-            credits = game.add.text(20, game.world.height - 70, 'PROGRAMMING & GRAPHICS: MIKKO JAKONEN\nMUSIC: OSKARI JAKONEN', creditStyle);
+            credits = this.game.add.text(20, this.game.world.height - 70, 'PROGRAMMING & GRAPHICS: MIKKO JAKONEN\nMUSIC: OSKARI JAKONEN', creditStyle);
             
-            soundSetting = game.add.text(game.world.centerX, game.world.centerY + 180, 'MUSIC: ON    ', gameStyle);
+            soundSetting = this.game.add.text(this.game.world.centerX, this.game.world.centerY + 180, 'MUSIC: ON    ', gameStyle);
             soundSetting.anchor.set(0.5);
             soundSetting.inputEnabled = true;
             
@@ -141,12 +135,12 @@
             _idleTween.stop();
             if(musicEnabled) music.play('', 0, 1, true);
             
-            game.time.events.loop(coinInterval, this.makeCoins, this);
+            this.game.time.events.loop(coinInterval, this.makeCoins, this);
             
-            game.add.tween(title).to({y: -80}, 800, Phaser.Easing.Cubic.Out, true);
-            game.add.tween(credits).to({y: game.world.height + 80}, 800, Phaser.Easing.Cubic.Out, true);
-            game.add.tween(instruction).to({alpha:0}, 2000, Phaser.Easing.Cubic.Out, true);
-            game.add.tween(soundSetting).to({alpha:0}, 800, Phaser.Easing.Linear.None, true);
+            this.game.add.tween(title).to({y: -80}, 800, Phaser.Easing.Cubic.Out, true);
+            this.game.add.tween(credits).to({y: this.game.world.height + 80}, 800, Phaser.Easing.Cubic.Out, true);
+            this.game.add.tween(instruction).to({alpha:0}, 2000, Phaser.Easing.Cubic.Out, true);
+            this.game.add.tween(soundSetting).to({alpha:0}, 800, Phaser.Easing.Linear.None, true);
         },
         
         //================={Updating:}=================//
@@ -165,7 +159,7 @@
                 if (coin.x < -10) coin.kill();
             }, this);
             
-            game.physics.arcade.collide(character, coins, this.collisionHandler, null, this);
+            this.game.physics.arcade.collide(character, coins, this.collisionHandler, null, this);
         },
         
         
@@ -186,11 +180,11 @@
             star.anchor.set(0.5);
             
             var x = 750;
-            var y = game.world.randomY;
+            var y = this.game.world.randomY;
             
             star.reset(x, y);
             
-            game.add.tween(star).to({x:-50, rotation: -1}, starsMoveSpeed, Phaser.Easing.Linear.None, true).onComplete.add(function() {
+            this.game.add.tween(star).to({x:-50, rotation: -1}, starsMoveSpeed, Phaser.Easing.Linear.None, true).onComplete.add(function() {
                 star.kill();
             });
         },
@@ -200,12 +194,12 @@
             coin.rotation = 0;
             
             var x = 750;
-            var y = game.world.randomY;
+            var y = this.game.world.randomY;
             
             coin.reset(x, y);
             
-            game.add.tween(coin).to({rotation: -Math.PI*4}, coinSpinSpeed, Phaser.Easing.Linear.None, true);
-            game.physics.enable(coin, Phaser.Physics.ARCADE);
+            this.game.add.tween(coin).to({rotation: -Math.PI*4}, coinSpinSpeed, Phaser.Easing.Linear.None, true);
+            this.game.physics.enable(coin, Phaser.Physics.ARCADE);
             coin.body.velocity.x = -coinMoveSpeed;
         },
         
@@ -244,9 +238,5 @@
             _sinking = true;
         }
     };
-    
-    
-    //start the game:
-    game = new Phaser.Game(700, 700, Phaser.CANVAS, 'snowman-game', snowman);
     
 })();
---
+++ src/preload.js
@@ -0,0 +1,64 @@
+(function() {
+    
+    Escape = { };
+
+    /**
+     * The Preloader state is responsible for all of the asset loading
+     * and showing of the load progress information.
+     * 
+     * @class Escape.Preloader
+     * @constructor
+     */
+    Escape.Preloader = function () {
+
+    };
+
+    Escape.Preloader.prototype = {    
+        /**
+         * The game preloader state creation.
+         * The idea is to specify here all of the other game states.
+         * 
+         * @public
+         */
+        create: function () {
+            this.state.add("Main", Escape.Main);
+        },
+
+        /**
+         * Updates the load progress information.
+         * 
+         * @public
+         */
+        loadUpdate: function () {
+            return;
+        },
+
+        /**
+         * Loads all of the game resources.
+         * 
+         * @public
+         */
+        preload: function () {
+            this.game.load.atlas('sprites', 'assets/walkingsprites.png', 'assets/walkingsprites_data.json');
+            
+            WebFont.load({
+                google: {
+                  families: ['VT323']
+                }
+            });
+        },
+
+        /**
+         * Starts the next state once the preloading has finished.
+         * 
+         * @public
+         */
+        update: function () {        
+            this.state.start("Main");
+        }
+    };
+    
+    var game = new Phaser.Game(1028, 720, Phaser.AUTO, 'escape-game');
+    game.state.add('Preload', Escape.Preloader, true);
+    
+})();
